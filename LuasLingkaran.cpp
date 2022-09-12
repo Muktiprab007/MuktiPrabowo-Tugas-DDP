f49a0ce0eac100b9313c4b14280bdd2d9347f0fc
Muktiprab007 <112567095+Muktiprab007@users.noreply.github.com>
2022-09-12 18:53:14 +0700

    Add files via upload

---
+++ LuasLingkaran.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float r, luas_lingkaran;
+
+    //jari-jari lingkaran
+    cout << "Jari-Jari Lingkaran =";
+    cin >> r;
+
+    //luas lingkaran
+    luas_lingkaran = 3.14 * r * r;
+
+    //keluaran
+    cout << "Luas Lingkaran = " << luas_lingkaran;
+
+    return 0;
+
+}
